14b972e521b2e1fc4ec7c5013989bd722bd580dc
Minal Gumgaonkar <minalgumgaonkar@Minals-MBP.hsd1.ca.comcast.net>
2020-01-28 21:53:20 -0800

    Updated

--- Missions/3. The Command Line/2. Command Line Intermediate/1. Working with Jupyter Console/script.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-	print("This is a Python script")
-
-print("Excellent!")
-s = 8
-
--- Missions/3. The Command Line/3. Git and Version Control/1. Introduction to Git/random_numbers/script.py
+++
@@ -1,3 +0,0 @@
-import random
-if __name__ == "__main__":
-    print(random.randint(0,10))
